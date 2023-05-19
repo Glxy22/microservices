89403a0b3d0019ab102201338003388d3ebc7547
Glxy22 <nadiatx2015@gmail.com>
2023-05-19 13:46:17 -0500

    Functional Interface exp

--- algorithm/src/main/java/AnonymousInnerClass/SupplierInterface.java
+++ algorithm/src/main/java/AnonymousInnerClass/SupplierInterface.java
@@ -1,16 +1,25 @@
 package AnonymousInnerClass;
 
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 public class SupplierInterface {
     public static void main(String[] args) {
-        SupplierInterface.show();
+//        SupplierInterface.show();
+        SupplierInterface.functionInterface();
     }
+    //supplier interface
     public static void show(){
         Supplier<Double> getRandom= () -> Math.random();
         var randomnum = getRandom.get();
         System.out.println(randomnum);
 
 
+    }
+    //functional interface
+    public static void functionInterface(){
+        Function<String,Integer> map= item-> item.length();
+        var length= map.apply("NADIA");
+        System.out.println(" 3 kinds of functional interface :" + length);
     }
 }
